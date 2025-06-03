41caf7c587f0e13bfe1f1c09433499b1be381c05
1901ankit <ankitsharmagzb1901@gmail.com>
2025-06-04 03:29:40 +0530

    sa

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'react-datepicker/dist/react-datepicker.css';
-import { store } from './app/store.js';
-import { Provider } from 'react-redux';
-createRoot(document.getElementById('root')).render(
- <Provider store={store}>
-    <App />
-    </Provider>
-  
-)
+import "./App.css";
+import Router from "./router";
+import { Toaster } from 'react-hot-toast';
+import { useEffect } from "react";
+
+function App() {
+
+  useEffect(() => {
+    (function (c, l, a, r, i, t, y) {
+      c[a] = c[a] || function () {
+        (c[a].q = c[a].q || []).push(arguments);
+      };
+      t = l.createElement(r);
+      t.async = 1;
+      t.src = "https://www.clarity.ms/tag/" + i;
+      y = l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "rtw1nvkd3c"); 
+  }, []);
+
+  return (
+    <>
+      <Router />
+      <Toaster position="top-center" reverseOrder={false} />
+    </>
+  );
+}
+
+export default App;
