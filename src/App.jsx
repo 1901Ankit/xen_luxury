618ba6924b718f8d322c6d4930d8a283011732a7
1901ankit <ankitsharmagzb1901@gmail.com>
2025-06-04 03:35:19 +0530

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,23 @@
 import "./App.css";
 import Router from "./router";
 import { Toaster } from 'react-hot-toast';
+import { useEffect } from "react";
+
 function App() {
+
+  useEffect(() => {
+    (function (c, l, a, r, i, t, y) {
+      c[a] = c[a] || function () {
+        (c[a].q = c[a].q || []).push(arguments);
+      };
+      t = l.createElement(r);
+      t.async = 1;
+      t.src = "https://www.clarity.ms/tag/" + i;
+      y = l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "rtw1nvkd3c"); 
+  }, []);
+
   return (
     <>
       <Router />
